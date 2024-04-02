73cf63af19b4263bbf8e805957bade59ad44590e
Paulmax27 <162152921+Paulmax27@users.noreply.github.com>
2024-04-02 21:46:15 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = ['15593840475']
+global.ownername = "Lalkhirhvela"//owner name
+global.ytname = ""
+global.socialm = ""
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Lalkhirhvela' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
